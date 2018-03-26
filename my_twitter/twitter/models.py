b7ab1293d4c2cd50cd391fe8c87218458ee0a273
buhaay <adrian.kodjasz@gmail.com>
2018-03-26 13:46:18 +0200

    added title and created_date

--- my_twitter/twitter/models.py
+++ my_twitter/twitter/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Tweet(models.Model):
+    title = models.CharField(max_length=256)
     content = models.CharField(max_length=256)
     creation_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
